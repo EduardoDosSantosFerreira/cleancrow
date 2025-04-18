f671cf4f853df1a0a144c4c2ad817ec5ce7f77a3
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-04-18 15:42:11 -0300

    Atualização automática: 2025-04-18 15:42:11

--- python/auto_commit.bat
+++ python/auto_commit.bat
@@ -1,3 +1,6 @@
 @echo off
-"C:\Users\Eduardo\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Eduardo\Documents\cleancrow\python\auto_commit.py"
+cd /d "C:\Users\Eduardo\Documents\cleancrow\python"
+echo ----------- %date% %time% ------------- >> log_autocommit.txt
+"C:\Users\Eduardo\AppData\Local\Programs\Python\Python313\python.exe" auto_commit.py >> log_autocommit.txt 2>&1
+echo ---------------------------------------- >> log_autocommit.txt
 exit
